da1c2a16e22d916a0179ff9e4616f27bf32441ac
heeyeon <heeyeon8662@gmail.com>
2024-12-27 15:05:14 +0900

    github actions ci/cd 환경 구축

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  build: {
+    outDir: "dist", // 빌드 폴더 이름 (기본값: 'dist')
+  },
+  base: "/", // S3에서 호스팅할 경우 root path 사용
 });
